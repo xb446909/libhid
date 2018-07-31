97cbc58e970d95258f10e590bbc8f52fa97112ba
ByronXiong <xiong-bin@live.com>
2018-07-31 21:07:28 +0800

    添加了测试工程，并完成读写测试

--- libhid/libhid.h
+++ libhid/libhid.h
@@ -1,1 +1,6 @@
 #pragma once
+
+void* __stdcall open_dev(unsigned short vid, unsigned short pid);
+void __stdcall close_dev(void* h);
+int __stdcall write_bytes(void* h, unsigned char* bytes, int count);
+int __stdcall read_bytes(void* h, unsigned char* bytes, int size);
